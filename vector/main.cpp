f4edd3be1513d3542f733a675f12b0b490b4480a
vvernovskaya <vvernovskaya@gmail.com>
2020-03-15 18:58:14 +0300

    update vector

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,41 +1,52 @@
 #include <iostream>
+#include <string>
 
 const int FIXED_CAP = 8;
 
+template <class T>
 class Vect {
 public:
     int capacity = FIXED_CAP;
-    int* array = new int [capacity];
+    T* array = new T [capacity];
     int size_v;
     Vect() {
         size_v = 0;
     }
-    explicit Vect(int n) {
+    Vect(int n, T val) {
         size_v = n;
-        capacity = (n / 8 + 1) * FIXED_CAP;
+        capacity = n;
+        for (int i = 0; i < size_v; i++) {
+            array[i] = val;
+        }
     }
+    ~Vect() {
+        delete[] array;
+    }
+
+    // Vect(const Vect &other) {}
+
     int operator[] (int i) {
         if (i >= size_v) {
-            std::cerr << "Index out of range";
+            std::cerr << "Index out of range" << std::endl;
             return 0;
         }
         else {
             return array[i];
         }
     }
-    int* begin() {
-        return &array[0];
-    }
-    int* end() {
-        return &array[size_v-1];
-    }
+
+    // int* begin() {}
+    // int* end() {}
+
     int size() {
         return size_v;
     }
+    T back() {
+        return array[size_v-1]
+    }
     void push_back(const int x) {
         if (size_v == capacity) {
-            capacity = capacity+FIXED_CAP;
-            int* new_array = new int[capacity];
+            T* new_array = new T[capacity*2];
             for (int i = 0; i < size_v; i++) {
                 new_array[i] = array[i];
             }
@@ -43,6 +54,7 @@ public:
             delete [] array;
             array = new_array;
             size_v++;
+            capacity *= 2;
         }
         else {
             array[size_v] = x;
@@ -50,9 +62,9 @@ public:
         }
     }
     void pop_back() {
-        if (size_v - 1 == capacity - FIXED_CAP) {
-            capacity = capacity-FIXED_CAP;
-            int* new_array = new int[capacity];
+        if (size_v - 1 == capacity / 2) {
+            capacity /= 2;
+            T* new_array = new T[capacity];
             for (int i = 0; i < size_v-1; i++) {
                 new_array[i] = array[i];
             }
@@ -61,7 +73,7 @@ public:
             size_v--;
         }
         else {
-            array[size_v-1] = 0;
+            delete &array[size_v-1];
             size_v--;
         }
     }
@@ -79,25 +91,31 @@ public:
 };
 
 int main() {
-    Vect a;
-    a.push_back(5);
-    a.push_back(8);
-    a.push_back(10);
-    a.push_back(19);
-    a.push_back(10);
-    a.push_back(9);
-    a.push_back(17);
-    a.push_back(14);
-    std::cout << "current capacity when size == 8 is " << a.capacity << std::endl;
-    a.push_back(10);
-    std::cout << "current capacity when size == 9 is " << a.capacity << std::endl;
+    // Initializing different vectors
+    Vect<int> a;
+    for (int i = 0; i < 10; i++) {
+        a.push_back(i);
+    }
     a.print();
+
+    Vect<int> b(10, 0);
+    b.print();
+
+    // Checking capacity
+    /* std::cout << "Current capacity of b when size_v is 10 : " << b.capacity << std::endl;
+    b.push_back(10);
+    b.print();
+    std::cout << "Current capacity of b when size_v is 11 : " << b.capacity << std::endl;
+    b.pop_back();
+    std::cout << "Current capacity of b when size_v is 10 : " << b.capacity << std::endl; */
+
+    std::cout << "Checking operator[]" << std::endl;
     for (int i = 0; i < a.size_v; i++) {
         std::cout << a.array[i] << " ";
     }
-    a.pop_back();
-    a.print();
     std::cout << std::endl;
-    std::cout << "current capacity when size == 8 is " << a.capacity << std::endl;
+
+    std::cout << "Deleting vector b" << std::endl;
+    b.~Vect();
     return 0;
 }
